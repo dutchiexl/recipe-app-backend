7c89e1800de182b995f4b630a59eefb5be37a749
nealvanmeert <neal.vanmeert@emuse.be>
2019-12-09 12:24:10 +0100

    Added build to heroku.

--- build/app.js
+++ build/app.js
@@ -2,10 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const server_1 = tslib_1.__importDefault(require("./api/server"));
-const server_2 = tslib_1.__importDefault(require("./assets/server"));
 const database_service_1 = require("./api/services/database.service");
 const dotenv_1 = tslib_1.__importDefault(require("dotenv"));
 dotenv_1.default.config();
 new database_service_1.DatabaseService();
-server_1.default.bootstrap().start(3000);
-server_2.default.bootstrap().start(3334);
+server_1.default.bootstrap().start(Number(process.env.PORT) || 3333);
+//AssetServer.bootstrap().start(3334);
